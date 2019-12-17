0cc23011d392bb683ee750e15aa860c4030ebc9c
Win Htet Aung <whaung1998@gmail.com>
2019-12-17 18:11:23 +0630

    trash file

--- autori.py
+++
@@ -1,7 +0,0 @@
-st = input()
-t = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-ans = ''
-for i in range(len(st)):
-	if st[i] in t:
-		ans = ans + st[i]
-print(ans)
